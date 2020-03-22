c6f386c2c139910f46720a45934f6456e86448ae
RafikFarhad <rafikfarhad@gmail.com>
2020-03-23 04:36:24 +0600

    JSON file check

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,15 @@ docker build -t $IMAGE_NAME . --file $INPUT_DOCKERFILE
 
 echo "Authenticating docker to gcloud"
 echo $INPUT_GCLOUD_SERVICE_KEY | python -m base64 -d > /tmp/key.json
-echo /tmp/key.json | docker login -u _json_key --password-stdin https://$INPUT_REGISTRY
+
+ls /tmp/key.json
+
+if [ -f "/tmp/key.json" ]; then
+    echo /tmp/key.json | docker login -u _json_key --password-stdin https://$INPUT_REGISTRY
+else
+    echo "JSON file not present. Exiting..."
+    exit 1
+fi
 
 echo "Pushing image"
 docker push $IMAGE_NAME
