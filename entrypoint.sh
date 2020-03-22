a0a07bff83eb8b2648708c284a7d8f5ecb6b88fd
RafikFarhad <rafikfarhad@gmail.com>
2020-03-23 02:48:16 +0600

    gcloud login updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,20 @@
 
 IMAGE_NAME="$INPUT_REGISTRY/$INPUT_PROJECT_NAME/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TAG"
 
-echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_REGISTRY/
+echo "Fully qualified image name: $IMAGE_NAME"
 
+echo $GCLOUD_SERVICE_KEY > /tmp/key.json
+
+echo "Activiting google cloud auth"
+gcloud auth activate-service-account --quiet --key-file /tmp/key.json
+
+echo "Configuring docker"
+gcloud auth configure-docker --quiet
+
+echo "Building image"
 docker build -t $IMAGE_NAME --file $INPUT_DOCKERFILE
 
+echo "Pushing image"
 docker push $IMAGE_NAME
+
+echo "Process complete."
