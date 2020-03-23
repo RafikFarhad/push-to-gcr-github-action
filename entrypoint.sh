5bdfc1504bf38189e74ead8fee910a32082537dd
RafikFarhad <rafikfarhad@gmail.com>
2020-03-23 13:09:15 +0600

    log in check

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,8 @@ echo $INPUT_GCLOUD_SERVICE_KEY | python -m base64 -d > /tmp/key.json
 
 cat /tmp/key.json | grep project_id
 
-if [ -f "/tmp/key.json" ]; then
-    echo /tmp/key.json | docker login -u _json_key --password-stdin https://$INPUT_REGISTRY
+if echo /tmp/key.json | docker login -u _json_key --password-stdin https://$INPUT_REGISTRY; then
+    echo "Logged in"
 else
     echo "JSON file not present. Exiting..."
     exit 1
@@ -23,5 +23,6 @@ echo "Pushing image"
 if ! docker push $IMAGE_NAME; then
     echo "Pushing failed. Exiting..."
 else
+    
     echo "Process complete."
 fi
