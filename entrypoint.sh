543e939aa03ccca93fdfcc489f925c3fcb60ffd0
RafikFarhad <rafikfarhad@gmail.com>
2020-03-23 13:40:45 +0600

    log in first build later

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,20 +4,22 @@ IMAGE_NAME="$INPUT_REGISTRY/$INPUT_PROJECT_NAME/$INPUT_IMAGE_NAME:$INPUT_IMAGE_T
 
 echo "Fully qualified image name: $IMAGE_NAME"
 
-echo "Building image"
-docker build -t $IMAGE_NAME . --file $INPUT_DOCKERFILE
-
 echo "Authenticating docker to gcloud"
 echo $INPUT_GCLOUD_SERVICE_KEY | python -m base64 -d > /tmp/key.json
 
 cat /tmp/key.json | grep project_id
 
+cat /tmp/key.json | grep private_key_id
+
 if echo /tmp/key.json | docker login -u _json_key --password-stdin https://$INPUT_REGISTRY; then
     echo "Logged in"
 else
-    echo "JSON file not present. Exiting..."
+    echo "Docker login failed. Exiting..."
     exit 1
 fi
+
+echo "Building image"
+docker build -t $IMAGE_NAME . --file $INPUT_DOCKERFILE
 
 echo "Pushing image"
 if ! docker push $IMAGE_NAME; then
