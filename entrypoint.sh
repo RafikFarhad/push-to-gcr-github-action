de296110bfe4cdce3c2ecd875ab7bc6c3d8e7fdc
Jeremy R DeYoung <jeremy@nextstep.guru>
2023-04-08 12:25:16 -0400

    adding docker_flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,25 +6,25 @@
 #version        :2.0.1
 #usage          :./entrypoint.sh
 #notes          :Required env values are: INPUT_REGISTRY,INPUT_PROJECT_ID,INPUT_IMAGE_NAME
-#                Optional env values are: INPUT_GCLOUD_SERVICE_KEY,INPUT_IMAGE_TAG,INPUT_DOCKERFILE,INPUT_TARGET,INPUT_CONTEXT,INPUT_BUILD_ARGS
+#                Optional env values are: INPUT_GCLOUD_SERVICE_KEY,INPUT_IMAGE_TAG,INPUT_DOCKERFILE,INPUT_TARGET,INPUT_CONTEXT,INPUT_BUILD_ARGS,INPUT_DOCKER_FLAGS
 #bash_version   :5.0.17(1)-release
 ###################################################
 
 ALL_IMAGE_TAG=()
 
 # detect service_account json flavour
-if [ $GOOGLE_APPLICATION_CREDENTIALS ] && ls $GOOGLE_APPLICATION_CREDENTIALS; then
+if [ "$GOOGLE_APPLICATION_CREDENTIALS" ] && ls "$GOOGLE_APPLICATION_CREDENTIALS"; then
     # workload identity
     echo "Workload identity found ..."
-    cp $GOOGLE_APPLICATION_CREDENTIALS /tmp/key.json
+    cp "$GOOGLE_APPLICATION_CREDENTIALS" /tmp/key.json
 else
-    if [ -z $INPUT_GCLOUD_SERVICE_KEY ]; then
+    if [ -z "$INPUT_GCLOUD_SERVICE_KEY" ]; then
         echo "GCLOUD_SERVICE_KEY is a required field when workload identity is not used. Exiting ..."
         exit 1
     fi
     # persing service account json
-    if ! echo $INPUT_GCLOUD_SERVICE_KEY | python3 -m base64 -d >/tmp/key.json 2>/dev/null; then
-        if ! echo $INPUT_GCLOUD_SERVICE_KEY >/tmp/key.json 2>/dev/null; then
+    if ! echo "$INPUT_GCLOUD_SERVICE_KEY" | python3 -m base64 -d >/tmp/key.json 2>/dev/null; then
+        if ! echo "$INPUT_GCLOUD_SERVICE_KEY" >/tmp/key.json 2>/dev/null; then
             echo "Failed to get gcloud_service_key. It could be plain text or base64 encoded service account JSON file"
             exit 1
         else
@@ -42,7 +42,7 @@ if ! gcloud auth login --cred-file=/tmp/key.json --quiet; then
     exit 1
 fi
 
-if gcloud auth configure-docker $INPUT_REGISTRY --quiet; then
+if gcloud auth configure-docker "$INPUT_REGISTRY" --quiet; then
     echo "Authentication successful to $INPUT_REGISTRY ..."
 else
     echo "Docker login failed. Exiting ..."
@@ -53,7 +53,7 @@ fi
 ALL_IMAGE_TAG=($(python3 -c "print(' '.join(list(set([v for v in [v.strip() for v in '$INPUT_IMAGE_TAG'.split(',')] if v]))))"))
 
 # default to 'latest' when $ALL_IMAGE_TAG is empty
-if [ ${#ALL_IMAGE_TAG[@]} -eq 0 ] ; then
+if [ ${#ALL_IMAGE_TAG[@]} -eq 0 ]; then
     echo "INPUT_IMAGE_TAG tag is not parsable. Using latest by default"
     ALL_IMAGE_TAG=(latest)
 fi
@@ -64,10 +64,10 @@ if [ "$INPUT_PUSH_ONLY" = true ]; then
     echo "Skipping image build ..."
 else
     echo "Building image ..."
-    [ -z $INPUT_TARGET ] && TARGET_ARG="" || TARGET_ARG="--target $INPUT_TARGET"
-    [ -z $INPUT_DOCKERFILE ] && FILE_ARG="" || FILE_ARG="--file $INPUT_DOCKERFILE"
+    [ -z "$INPUT_TARGET" ] && TARGET_ARG="" || TARGET_ARG="--target $INPUT_TARGET"
+    [ -z "$INPUT_DOCKERFILE" ] && FILE_ARG="" || FILE_ARG="--file $INPUT_DOCKERFILE"
 
-    if [ ! -z "$INPUT_BUILD_ARGS" ]; then
+    if [ -n "$INPUT_BUILD_ARGS" ]; then
         for ARG in $(echo "$INPUT_BUILD_ARGS" | tr ',' '\n'); do
             BUILD_PARAMS="$BUILD_PARAMS --build-arg ${ARG}"
         done
@@ -75,7 +75,7 @@ else
 
     echo "docker build $BUILD_PARAMS $TARGET_ARG -t $TEMP_IMAGE_NAME $FILE_ARG $INPUT_CONTEXT"
 
-    if docker build $BUILD_PARAMS $TARGET_ARG -t $TEMP_IMAGE_NAME $FILE_ARG $INPUT_CONTEXT; then
+    if docker build "$INPUT_DOCKER_FLAGS" "$BUILD_PARAMS" '$TARGET_ARG' -t "$TEMP_IMAGE_NAME" "$FILE_ARG" "$INPUT_CONTEXT"; then
         echo "Image built ..."
     else
         echo "Image building failed. Exiting ..."
@@ -83,7 +83,7 @@ else
     fi
 fi
 
-for IMAGE_TAG in ${ALL_IMAGE_TAG[@]}; do
+for IMAGE_TAG in "${ALL_IMAGE_TAG[@]}"; do
 
     IMAGE_NAME="$INPUT_REGISTRY/$INPUT_PROJECT_ID/$INPUT_IMAGE_NAME:$IMAGE_TAG"
 
@@ -91,11 +91,11 @@ for IMAGE_TAG in ${ALL_IMAGE_TAG[@]}; do
 
     echo "Creating docker tag ..."
 
-    docker tag $TEMP_IMAGE_NAME $IMAGE_NAME
+    docker tag "$TEMP_IMAGE_NAME" "$IMAGE_NAME"
 
     echo "Pushing image $IMAGE_NAME ..."
 
-    if ! docker push $IMAGE_NAME; then
+    if ! docker push "$IMAGE_NAME"; then
         echo "Pushing failed. Exiting ..."
         exit 1
     else
